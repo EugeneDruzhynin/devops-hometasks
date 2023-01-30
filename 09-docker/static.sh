2be2dca49bf456432440da77b3631ef483bdea59
EugeneDruzhynin <eu.druzhynin@gmail.com>
2023-01-30 14:19:15 +0300

    create new folder

--- 09-docker/static.sh
+++ 09-docker/static.sh
@@ -1,4 +1,2 @@
 #!/bin/bash
-docker run -d --name site2 -p 8081:80 hometask-image
-docker exec -d site2 rm /var/www/html/index.php
-docker cp index.html site2:/var/www/html
+docker run -d --name site2 -p 8081:80 -v 'pwd'/index.html:/var/www/html/index.php hometask-image
