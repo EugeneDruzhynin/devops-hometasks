de3670a9408abc111c13c4c3f266786b2787127e
EygeneDruzhynin <eu.druzhynin@gmail.com>
2023-02-15 18:11:21 +0300

    Managed by Terraform

---
+++ 13-terraform/github/variables.tf
@@ -0,0 +1,13 @@
+variable "files" {
+    default = [
+        "Deployment.tf",
+        "Namespace.tf",
+        "provider.tf",
+        "Service.tf",
+        "Ingress.tf",
+        "terraform.tfvars",
+        "github/catalog.tf",
+        "github/provider.tf",
+        "github/variables.tf"
+    ]
+}
