3c383a94c47834d4f0e1218eb882460d2819867d
EygeneDruzhynin <eu.druzhynin@gmail.com>
2023-02-15 18:09:01 +0300

    Managed by Terraform

---
+++ 13-terraform/github/provider.tf
@@ -0,0 +1,13 @@
+variable "tf_token" {}
+terraform {
+  required_providers {
+    github = {
+      source  = "integrations/github"
+      version = "~> 5.0"
+    }
+  }
+}
+
+provider "github" {
+    token = var.tf_token
+}
